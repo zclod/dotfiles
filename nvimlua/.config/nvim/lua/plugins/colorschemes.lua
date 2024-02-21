39455761ffda718684b5ef255ad73417edb6a926
zclod <zclod@darklabs.it>
2024-02-21 16:11:55 +0100

    colorschemes update

--- nvimlua/.config/nvim/lua/plugins/colorschemes.lua
+++ nvimlua/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,14 @@
 return {
+
   'folke/tokyonight.nvim',
   'neanias/everforest-nvim',
   'shaunsingh/moonlight.nvim',
   'shaunsingh/nord.nvim',
-
-  --Solarized
-  'romainl/flattened',
+  'EdenEast/nightfox.nvim',
+  'nyoom-engineering/oxocarbon.nvim',
+  'rebelot/kanagawa.nvim',
+  'loctvl842/monokai-pro.nvim',
+  {'catppuccin/nvim', name = 'catppuccin'},
+  'romainl/flattened',  --Solarized
 
 }
