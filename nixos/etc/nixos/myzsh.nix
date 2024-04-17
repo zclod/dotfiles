4d5561295d5a87387cc0d2b6884efdfb15cbc7d6
zclod <zclod@darklabs.it>
2024-04-17 20:07:14 +0200

    zsh

--- nixos/etc/nixos/myzsh.nix
+++ nixos/etc/nixos/myzsh.nix
@@ -1,21 +1,21 @@
 {pkgs, ...}:
 {
 
-    environment.systemPackages = with pkgs; [fzf-zsh];
-
     users.defaultUserShell = pkgs.zsh;
     programs.zsh.enable = true;
     programs.zsh.autosuggestions.enable = true;
     programs.zsh.enableCompletion = true;
     programs.zsh.syntaxHighlighting.enable = true;
     programs.zsh.ohMyZsh.enable = true;
-    programs.zsh.ohMyZsh.plugins = [ "git" "colored-man-pages" "command-not-found" "extract" ];
+    programs.zsh.ohMyZsh.plugins = [ "git" "colored-man-pages" "command-not-found" "extract" "fzf" ];
     programs.zsh.ohMyZsh.theme = "avit";
 
 
     programs.zsh.shellAliases = {
         e = "nvim";
         se = "sudoedit";
+        lg = "lazygit";
+        f = ''cd "$(xplr --print-pwd-as-result)"'';
     };
 
     environment.variables = {
