7db32eb89d22246a7a7513340ef3d3fe0f863de4
zclod <zclod@darklabs.it>
2025-04-03 10:47:52 +0200

    yarepl nvim

--- nvimlua/.config/nvim/lua/plugins/yarepl.lua
+++ nvimlua/.config/nvim/lua/plugins/yarepl.lua
@@ -28,5 +28,14 @@ return {
       {"<Leader>rv", mode = {"v"}, '<Plug>(REPLSendVisual)', desc = "send [v]isual selection to repl"},
       {"<Leader>re", mode = {"n"}, '<Plug>(REPLExec)', desc = "[e]xec command in repl"},
       {"<Leader>rt", mode = {"n"}, '<cmd>REPLStart bash<cr>', desc = "start a [t]erminal repl for the current buffer"},
+
+      {"<Leader>aa", mode = {"n"}, '<cmd>REPLStart aider<cr>', desc = "start [a]ider repl"},
+      {"<Leader>ae", mode = {"n"}, '<Plug>(AiderExec)', desc = "Type the prompt in cmdline and send it to aider"},
+      {"<Leader>ay", mode = {"n"}, '<Plug>(AiderSendYes)', desc = "send [y]es to aider"},
+      {"<Leader>an", mode = {"n"}, '<Plug>(AiderSendNo)', desc = "send [n]o to aider"},
+      {"<Leader>ap", mode = {"n"}, '<Plug>(AiderSendPaste)', desc = "send [p]aste command to aider"},
+      {"<Leader>al", mode = {"n"}, '<Plug>(REPLSendLine-aider)', desc = "send current [l]ine to aider"},
+      {"<Leader>av", mode = {"v"}, '<Plug>(REPLSendVisual-aider)', desc = "send [v]isual selection to aider"},
+      {"<Leader>ac", mode = {"n"}, '<Plug>(AiderSendExit)', desc = "[c]lose aider"},
     },
 }
